47791756f49ed3a80a4ecee0892dbc016355e9c5
may-thu-htet <maythuhtet2892@gmail.com>
2022-03-27 15:37:26 +0900

    deep dive into components

---
+++ vue/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <!-- <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goal>
+      <template v-slot:default="item">
+        <h2>{{ item.goal }}</h2>
+        <p>{{ item.anotherProp }}</p>
+        <h4>{{ item }}</h4>
+      </template>
+    </course-goal> -->
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goal')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goal')">Manage Goals</button>
+    <!-- <active-goal v-if="selectedComponent === 'active-goal'"></active-goal>
+    <manage-goal v-if="selectedComponent === 'manage-goal'"></manage-goal> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoal from "./components/CourseGoal.vue";
+import ActiveGoal from "./components/ActiveGoal.vue";
+import ManageGoal from "./components/ManageGoal.vue";
+export default {
+  components: {
+    TheHeader,
+    ActiveGoal,
+    ManageGoal,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goal",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vue/cmp-adv-01-starting-setup/src/components/ManageGoal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Manage Goal</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <ErrorAlert v-if="inputIsInvalid">
+        <h2>Input is Invalid</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </ErrorAlert>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+  components: { ErrorAlert },
+};
+</script>

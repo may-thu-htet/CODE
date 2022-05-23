6f9064dd194d35704720e8ae220ae3391145cccc
may-thu-htet <maythuhtet2892@gmail.com>
2022-05-23 20:44:43 +0900

    http request

--- vue/http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ vue/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,17 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Please fill the survey first.
+      </p>
+
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +29,49 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(
+        'https://vue-http-demo-759bf-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const result = [];
+          for (const id in data) {
+            result.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = result;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Fail to fetch. Try again later.';
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>

6436a92af378d27fd6a18fd156fb114f499df016
may-thu-htet <maythuhtet2892@gmail.com>
2022-06-02 15:57:00 +0900

    vuex exe

---
+++ vue/vuex-11-a-challenge-starting-code/src/store/index.js
@@ -0,0 +1,38 @@
+import { createStore } from 'vuex';
+import productModules from './modules/product.js';
+import cartModules from './modules/cart.js';
+
+const store = createStore({
+  modules: {
+    prods: productModules,
+    cart: cartModules,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logOut(context) {
+      context.commit('logout');
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
